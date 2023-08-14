c310ef1278e7e5741c3272ae6ca04cc31993bbfb
GorCame <142260786+GorCame@users.noreply.github.com>
2023-08-14 22:43:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Merhaba")
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": "Bir şakaya karşılık cevap",
+            "SHEESH":  "Onaylamamak",
+            "CREEPY":  "Korkunç"
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict.)
+else:
+    print("Henüz Ekleyemedik Maalesef.")
+    
+#HOCAM KULLANICININ SEÇİP BİLGİSAYARIN CEVAP VERMESİNİ EKLEYEMEDİM.
